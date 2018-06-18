268043955940e6f091c4baaeb98f72828917bf7e
Andriy Maletsky <andriy.maletsky@gmail.com>
2018-06-18 12:48:57 +0300

    fix test2 data slicing

--- test/task_2/fixtures/__init__.py
+++ test/task_2/fixtures/__init__.py
@@ -1,8 +1,9 @@
 import os
 import json
+from collections import OrderedDict
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 with open(os.path.join(__location__, 'data.json')) as f:
-    fixtures = json.load(f)
+    fixtures = json.load(f, object_pairs_hook=OrderedDict)
